9d9549386c1f33ec6f95400c43cb06271ff65c64
SeungminYun-G-B <147472555+SeungminYun-G-B@users.noreply.github.com>
2023-10-29 17:37:10 +0900

    마찰 코드로 변경

--- Particle.js
+++ Particle.js
@@ -1,40 +1,54 @@
-class Particle {
-  constructor() {
-    this.pos = createVector(width/2, height/2);
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
 
-    this.c = color(0);
-    this.w = 50;
+class Mover {
+  constructor(x, y, m) {
+    this.mass = m;
+    this.radius = m * 8;
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
   }
 
-  addForce(aForce) {
-    this.acc.add(aForce);
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
   }
 
   update() {
-    this.checkEdge();
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-
-    this.acc.set(0, 0);
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
   }
-
-
-  checkEdge() {
-    if (this.pos.y > height) {
-      this.vel.y = this.vel.y * -0.8;
-      this.pos.y = height;
-    }
-    
-    if (this.pos.x > width) {
-      this.pos.x = 0;
-    }
-  }
-
 
   show() {
-    fill(this.c);
-    circle(this.pos.x, this.pos.y, this.w);
+    stroke(0);
+    strokeWeight(2);
+    fill(127, 127);
+    circle(this.position.x, this.position.y, this.radius * 2);
   }
+
+  contactEdge() {
+    // The mover is touching the edge when it's within one pixel
+    return (this.position.y > height - this.radius - 1);
+  }
+
+  bounceEdges() {
+    // A new variable to simulate an inelastic collision
+    // 10% of the velocity's x or y component is lost
+    let bounce = -0.9;
+    if (this.position.x > width - this.radius) {
+      this.position.x = width - this.radius;
+      this.velocity.x *= bounce;
+    } else if (this.position.x < this.radius) {
+      this.position.x = this.radius;
+      this.velocity.x *= bounce;
+    }
+    if (this.position.y > height - this.radius) {
+      this.position.y = height - this.radius;
+      this.velocity.y *= bounce;
+    }
+  }
+
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,39 @@
-let ball;
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+let mover;
 
 function setup() {
-  createCanvas(400, 800);
-
-  ball = new Particle();
+  createCanvas(640, 240);
+  mover = new Mover(width / 2, 30, 5);
+  createP('Click mouse to apply wind force.');
 }
 
-
 function draw() {
-  background(220);
+  background(255);
 
-  let gravity = createVector(0, 0.3);
-  ball.addForce(gravity);
+  let gravity = createVector(0, 1);
+  //{!1} I should scale by mass to be more accurate, but this example only has one circle
+  mover.applyForce(gravity);
 
-  ball.update();
-  ball.show();
+  if (mouseIsPressed) {
+    let wind = createVector(0.5, 0);
+    mover.applyForce(wind);
+  }
+
+  if (mover.contactEdge()) {
+    //{!5 .bold}
+    let c = 0.1;
+    let friction = mover.velocity.copy();
+    friction.mult(-1);
+    friction.setMag(c);
+
+    //{!1 .bold} Apply the friction force vector to the object.
+    mover.applyForce(friction);
+  }
+
+  mover.bounceEdges();
+  mover.update();
+  mover.show();
 }
